ea0fa46acf7ee1a29a9ac4af7ef9512ffb744991
Rodrigo Ferraz <rocferraz@gmail.com>
2026-05-03 15:59:04 -0300

    Projeto Inicial

---
+++ DotnetSample.Api/Calculo/OperacoesMatematicas.cs
@@ -0,0 +1,15 @@
+namespace DotnetSample.Api.Calculo
+{
+    public class OperacoesMatematicas
+    {
+        public double Somar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Diminuir(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
