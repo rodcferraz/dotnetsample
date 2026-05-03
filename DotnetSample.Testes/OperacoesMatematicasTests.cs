d3c5b5ad2ad2e8bb1c248df9c7a666cbdb1a1632
Rodrigo Ferraz <rocferraz@gmail.com>
2026-05-03 16:16:57 -0300

    Testes de operacoesMatematicas

---
+++ DotnetSample.Testes/OperacoesMatematicasTests.cs
@@ -0,0 +1,44 @@
+using DotnetSample.Api.Calculo;
+
+namespace DotnetSample.Testes;
+
+public class OperacoesMatematicasTests
+{
+    [Fact]
+    public void Somar_DeveRetornarSomaDosDoisValores()
+    {
+        var sut = new OperacoesMatematicas();
+
+        var resultado = sut.Somar(5.5, 4.5);
+
+        Assert.Equal(10.0, resultado, precision: 5);
+    }
+
+    [Theory]
+    [InlineData(10.0, 5.0, 5.0)]
+    [InlineData(0.0, 0.0, 0.0)]
+    [InlineData(-3.0, -2.0, -1.0)]
+    [InlineData(2.5, 1.25, 1.25)]
+    public void Diminuir_DeveRetornarDiferencaDosDoisValores(double valor, double valorSubtrair, double esperado)
+    {
+        var sut = new OperacoesMatematicas();
+
+        var resultado = sut.Diminuir(valor, valorSubtrair);
+
+        Assert.Equal(esperado, resultado, precision: 5);
+    }
+
+    [Theory]
+    [InlineData(2.0, 3.0, 5.0)]
+    [InlineData(-2.0, 3.0, 1.0)]
+    [InlineData(2.5, -1.5, 1.0)]
+    [InlineData(0.0, 0.0, 0.0)]
+    public void Somar_DeveTrabalharComValoresPositivosENegativos(double a, double b, double esperado)
+    {
+        var sut = new OperacoesMatematicas();
+
+        var resultado = sut.Somar(a, b);
+
+        Assert.Equal(esperado, resultado, precision: 5);
+    }
+}
